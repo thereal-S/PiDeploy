bc5fe683de706f160b5becacfb9f83dc63d68e44
thereal-S <86622551+thereal-S@users.noreply.github.com>
2021-08-13 09:55:38 +0200

    updated for virtual box

--- PiDeployBasic.sh
+++ PiDeployBasic.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 sudo apt-get update -y && sudo apt-get upgrade -y
+#virtualBox
+sudo apt-get install build-essential
+sudo apt-get install virtualbox-guest-utils
+#
 sudo apt-get install git
 sudo apt-get install wget
 sudo apt-get install curl
 sudo apt-get install fish
+sudo chsh -s 'which fish'
 sudo reboot
